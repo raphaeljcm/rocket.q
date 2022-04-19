549bca5380007a8360c7686b86917a0d86e80372
Raphael Marques <raphaeljcmarques@gmail.com>
2022-04-18 22:41:41 -0300

    Adding a few pages and styles

--- src/app.js
+++ src/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/home.html'))
+});
+
+app.get('/create-pass', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/create-pass.html'))
+});
+
+app.get('/room', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/room.html'))
+});
 
 app.listen(3000, () => console.log('Server running'));
